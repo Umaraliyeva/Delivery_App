2f18ab9405586c62208c68c8407fa4174ed574ea
umaraliyevashoxsanam <jetbrains085@gmail.com>
2025-02-22 00:14:53 +0530

    Deleted register.html

--- src/main/java/org/example/delivery_app/controller/AuthController.java
+++ src/main/java/org/example/delivery_app/controller/AuthController.java
@@ -3,11 +3,7 @@ package org.example.delivery_app.controller;
 import jakarta.servlet.annotation.MultipartConfig;
 import lombok.RequiredArgsConstructor;
 import org.example.delivery_app.dto.LoginDTO;
-import org.example.delivery_app.entity.Attachment;
-import org.example.delivery_app.entity.AttachmentContent;
-import org.example.delivery_app.entity.Role;
 import org.example.delivery_app.entity.User;
-import org.example.delivery_app.enums.RoleName;
 import org.example.delivery_app.repo.AttachmentContentRepository;
 import org.example.delivery_app.repo.AttachmentRepository;
 import org.example.delivery_app.repo.RoleRepository;
@@ -15,7 +11,6 @@ import org.example.delivery_app.repo.UserRepository;
 import org.example.delivery_app.service.EmailService;
 import org.example.delivery_app.service.EmailVerificationService;
 import org.example.delivery_app.service.TokenService;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -23,14 +18,10 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
 import javax.mail.MessagingException;
-import java.io.IOException;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Random;
+
 
 @RestController
 @RequestMapping("/auth")
@@ -40,46 +31,14 @@ import java.util.Random;
 public class AuthController {
 
 
-    private final RoleRepository roleRepository;
+
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
-    private final AttachmentRepository attachmentRepository;
-    private final AttachmentContentRepository attachmentContentRepository;
     private final EmailVerificationService emailVerificationService;
     private final EmailService emailService;
 
-    @PostMapping("/register")
-    public String registerUser(
-            @RequestParam("username") String username,
-            @RequestParam("fullName") String fullName,
-            @RequestParam("password") String password,
-            @RequestParam("file") MultipartFile file) throws IOException {
-
-        Attachment attachment = Attachment.builder()
-                .fileName(file.getOriginalFilename())
-                .build();
-        attachmentRepository.save(attachment);
-
-        AttachmentContent attachmentContent = AttachmentContent.builder()
-                .content(file.getBytes())
-                .attachment(attachment)
-                .build();
-        attachmentContentRepository.save(attachmentContent);
-
-        Role userRole = roleRepository.findByRoleName(RoleName.ROLE_USER).orElseThrow();
-        User user = User.builder()
-                .username(username)
-                .password(passwordEncoder.encode(password))
-                .fullName(fullName)
-                .attachment(attachment)
-                .roles(List.of(userRole))
-                .build();
-        userRepository.save(user);
-
-        return "User registered successfully";
-    }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginDTO loginDTO) {
